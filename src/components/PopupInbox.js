4d53acf208813316238bae6c6563d4094572cfec
ilona <ilona.avdiushkina@gmail.com>
2021-02-24 22:05:35 +0100

    added popup visibility for 3 navigation buttons

---
+++ src/components/PopupInbox.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/popupOptions.css';
+
+
+function PopupInbox() {
+    return (
+        <div className="popup">
+            <h2>Inbox</h2>
+            <p>Share ideas with your friends</p>
+            <p className="search">SEARCH BAR</p>
+            <p>Suggested</p>
+            <ul>
+                <li>Person</li>
+                <li>Person</li>
+                <li>Person</li>
+                <li>Person</li>
+            </ul>
+        </div>
+    )
+}
+
+export default PopupInbox
---
+++ src/components/PopupUpdates.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/popupOptions.css';
+
+
+function PopupUpdates() {
+    return (
+        <div className="popup">
+            <h3>Updates</h3>
+            <div className="infoContainer">
+                <p>Some info</p>
+                <div>Pictures</div>
+            </div>
+        </div>
+    )
+}
+
+export default PopupUpdates
