a1a767ddc81f68dbdfc3a9f77ea3b9040861c926
ilona <ilona.avdiushkina@gmail.com>
2021-02-24 21:22:49 +0100

    useState for account popup

---
+++ src/components/PopupOptions.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/popupOptions.css';
+
+
+function PopupOptions() {
+    return (
+        <div className="popup">
+            <h3>Accounts</h3>
+            <ul>
+                <li>Add another account</li>
+                <li>Unlock business tools</li>
+            </ul>
+            <h3>More options</h3>
+            <ul>
+                <li>Settings</li>
+                <li>Tune your home feed</li>
+                <li>Get help</li>
+                <li>See terms and privacy</li>
+                <li>Log out</li>
+            </ul>
+        </div>
+    )
+}
+
+export default PopupOptions
