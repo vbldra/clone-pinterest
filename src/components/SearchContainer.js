d77ab14b71d3ce31a962d6ab181e980ef206cab6
ilona <ilona.avdiushkina@gmail.com>
2021-03-09 09:18:58 +0100

    font awesome component and search bar

---
+++ src/components/SearchContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DataApi from "./DataApi";
+
+
+function SearchContainer() {
+    return (
+        <div className="container">
+            <p>Your search...</p>
+            <br/>
+            <p>SOME TEXT</p>
+            {/* <DataApi type="search"/> */}
+        </div>
+        )
+}
+
+export default SearchContainer
