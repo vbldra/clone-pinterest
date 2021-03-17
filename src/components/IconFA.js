30ce4b2630aef5087e002c6b6f1fd549ded7f1f4
ilona <ilona.avdiushkina@gmail.com>
2021-03-17 16:37:34 +0100

    navigation but context doesnt work

--- src/components/IconFA.js
+++ src/components/IconFA.js
@@ -2,26 +2,28 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faQuestion, faBell, faChevronDown, faCommentDots, faStickyNote, faUser, faSearch} from '@fortawesome/free-solid-svg-icons'
 
-function IconFA(props) {
-    switch (props.type) {
-        case "logo":
-            return <FontAwesomeIcon className="icon logo" icon={faStickyNote} />    
-        case "search":
-            return <FontAwesomeIcon className="icon search-icon" icon={faSearch} />
-        case "updates":
-            return <FontAwesomeIcon className="icon" icon={faBell} />
-        case "inbox":
-            return <FontAwesomeIcon className="icon" icon={faCommentDots} />
-        case "user":
-            return <FontAwesomeIcon className="icon" icon={faUser} />
-        case "arrow":
-            return <FontAwesomeIcon className="icon" icon={faChevronDown} />
-        case "plus":
-            return <FontAwesomeIcon className="icon footer-btn plus-btn" icon={faPlus} />
-        case "question":
-            return <FontAwesomeIcon className="icon footer-btn question-btn" icon={faQuestion} />
-        default:
-            break;
+class IconFA extends React.Component {
+    render = () => {
+        switch (this.props.type) {
+            case "logo":
+                return <FontAwesomeIcon className="icon logo" icon={faStickyNote} />    
+            case "search":
+                return <FontAwesomeIcon className="icon search-icon" icon={faSearch} />
+            case "updates":
+                return <FontAwesomeIcon className="icon" icon={faBell} />
+            case "inbox":
+                return <FontAwesomeIcon className="icon" icon={faCommentDots} />
+            case "user":
+                return <FontAwesomeIcon className="icon" icon={faUser} />
+            case "arrow":
+                return <FontAwesomeIcon className="icon" icon={faChevronDown} />
+            case "plus":
+                return <FontAwesomeIcon className="icon footer-btn plus-btn" icon={faPlus} />
+            case "question":
+                return <FontAwesomeIcon className="icon footer-btn question-btn" icon={faQuestion} />
+            default:
+                break;
+        }
     }
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,96 +8,105 @@ import PopupOptions from "./PopupOptions";
 import PopupUpdates from "./PopupUpdates";
 import PopupInbox from "./PopupInbox";
 
-function Navigation() {
-    const { dispatch, popups, searchText, setSearchText } = useContext(
-        MessengerPiggeon
-    );
-    const history = useHistory();
 
-    function handleSubmit(event) {
+class Navigation extends React.Component {
+    static contextType = MessengerPiggeon
+    // const { dispatch, popups, searchText, setSearchText } = useContext(
+    //     MessengerPiggeon
+    // );
+    // const history = useHistory();
+
+    componentDidMount() {
+        const mesPig = this.context
+    }
+
+
+    handleSubmit = (event) => {
         event.preventDefault();
     }
 
-    function handleChange(event) {
-        history.push("/search");
-        setSearchText(event.target.value);
+    handleChange = (event) => {
+        this.props.history.push("/search");
+        this.mesPig.setSearchText(event.target.value);
     }
+
+    render = () => {
+        return (
+            <nav>
+                <div className="nav-left">
+                    <NavLink className="nav-item" exact to="/">
+                        <IconFA type="logo" />
+                    </NavLink>
     
-    return (
-        <nav>
-            <div className="nav-left">
-                <NavLink className="nav-item" exact to="/">
-                    <IconFA type="logo" />
-                </NavLink>
-
-                <NavLink
-                    className="nav-item"
-                    activeClassName="active"
-                    exact
-                    to="/"
-                >
-                    Home
-                </NavLink>
-
-                <NavLink
-                    className="nav-item"
-                    activeClassName="active"
-                    exact
-                    to="/following"
-                >
-                    Following
-                </NavLink>
-            </div>
-
-            <div className="search-bar">
-                <form onSubmit={handleSubmit}>
-                    <input
-                        onChange={handleChange}
-                        value={searchText}
-                        type="text"
-                        placeholder="Search"
-                    />
-                </form>
-            </div>
-
-            <div className="nav-right">
-                <button
-                    className="nav-item"
-                    onClick={() => {
-                        dispatch({ type: "popupUpdatesVisible" });
-                    }}
-                >
-                    <IconFA type="updates" />
-                </button>
-                {popups.popupUpdatesVisible && <PopupUpdates />}
-
-                <button
-                    className="nav-item"
-                    onClick={() => {
-                        dispatch({ type: "popupInboxVisible" });
-                    }}
-                >
-                    <IconFA type="inbox" />
-                </button>
-                {popups.popupInboxVisible && <PopupInbox />}
-
-                {/* {user.username && 
-                    <NavLink className="nav-item" exact to="/USER_NAME/_saved/">
-                        <IconFA type="user" />
-                    </NavLink> 
-                } */}
-
-                {/* <button
-                    onClick={() => {
-                        dispatch({ type: "popupOptionVisible" });
-                    }}
-                >
-                    <IconFA type="arrow" />
-                </button>
-                {popups.popupOptionVisible && <PopupOptions />} */}
-            </div>
-        </nav>
-    );
+                    <NavLink
+                        className="nav-item"
+                        activeClassName="active"
+                        exact
+                        to="/"
+                    >
+                        Home
+                    </NavLink>
+    
+                    <NavLink
+                        className="nav-item"
+                        activeClassName="active"
+                        exact
+                        to="/following"
+                    >
+                        Following
+                    </NavLink>
+                </div>
+    
+                <div className="search-bar">
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                            onChange={this.handleChange}
+                            value={this.mesPig.searchText}
+                            type="text"
+                            placeholder="Search"
+                        />
+                    </form>
+                </div>
+    
+                <div className="nav-right">
+                    <button
+                        className="nav-item"
+                        // onClick={() => {
+                        //     dispatch({ type: "popupUpdatesVisible" });
+                        // }}
+                    >
+                        <IconFA type="updates" />
+                    </button>
+                    {this.mesPig.popups.popupUpdatesVisible && <PopupUpdates />}
+    
+                    <button
+                        className="nav-item"
+                        // onClick={() => {
+                        //     dispatch({ type: "popupInboxVisible" });
+                        // }}
+                    >
+                        <IconFA type="inbox" />
+                    </button>
+                    {this.mesPig.popups.popupInboxVisible && <PopupInbox />}
+    
+                    {/* {user.username && 
+                        <NavLink className="nav-item" exact to="/USER_NAME/_saved/">
+                            <IconFA type="user" />
+                        </NavLink> 
+                    } */}
+    
+                    {/* <button
+                        onClick={() => {
+                            dispatch({ type: "popupOptionVisible" });
+                        }}
+                    >
+                        <IconFA type="arrow" />
+                    </button>
+                    {popups.popupOptionVisible && <PopupOptions />} */}
+                </div>
+            </nav>
+        );
+    }
 }
 
 export default Navigation;
